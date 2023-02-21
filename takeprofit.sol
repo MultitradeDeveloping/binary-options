27c59d5a72b40b9a0f15da7b3bcf27ee37a8ceb3
Mulrirtade <111198070+MultitradeDeveloping@users.noreply.github.com>
2023-02-21 23:40:27 +0200

    Create takeprofit.sol

---
+++ takeprofit.sol
@@ -0,0 +1,36 @@
+// import "./strings.sol";
+// SPDX-License-Identifier: MIT
+import "./fillorders.sol";
+pragma solidity ^0.8.0;
+
+
+
+
+contract takeprofit is orderSystem{
+uint constant fee = 4;
+
+function take(bool up) internal{
+if(up == true){
+    uint i = 0;
+    while (i<id){
+        i++;
+        address payable addr = payable(idBase[i]);
+        uint coef = bidIds[i];
+        uint val = filledBids[i][coef];
+        addr.transfer(val*(coef/1000+1)*(100-fee)/100);
+    }
+}
+
+else{
+    uint i = 0;
+    while (i<id){
+        i++;
+        address payable addr = payable(idBase[i]);
+        uint coef = askIds[i];
+        uint val = filledAsks[i][coef];
+        addr.transfer(val*(coef/1000+1)*(100-fee)/100);
+    }
+}
+
+}
+}
