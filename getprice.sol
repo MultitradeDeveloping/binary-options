7943b5f8a13c056603c985566e0c5cb33c38bbd5
Mulrirtade <111198070+MultitradeDeveloping@users.noreply.github.com>
2023-03-02 23:15:06 +0200

    Update getprice.sol

--- getprice.sol
+++ getprice.sol
@@ -1,16 +1,10 @@
-// import "./strings.sol";
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Automaticaly {
     
     AggregatorV3Interface internal BTCpriceFeed;
     AggregatorV3Interface internal ETHpriceFeed;
     AggregatorV3Interface internal LinkpriceFeed;
-    uint n = 0;
+    uint public n = 0;
     uint public BTCUSD;
     uint time;
     constructor() {
@@ -18,7 +12,7 @@ contract Automaticaly {
         ETHpriceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
         LinkpriceFeed = AggregatorV3Interface(0x48731cF7e84dc94C5f84577882c14Be11a5B7456);
     }
-    function LatestBTCprice() internal{
+    function LatestBTCprice() public{
         (
             uint80 roundID,
             int price,
@@ -26,7 +20,6 @@ contract Automaticaly {
             uint timeStamp,
             uint80 answeredInRound
         ) = BTCpriceFeed.latestRoundData();
-        n++;
         BTCUSD = uint(price);
         time = timeStamp;
         // time[n] = uint80(startedAt);
@@ -37,25 +30,9 @@ contract Automaticaly {
 
 
 contract GetPrice is Automaticaly {
-    
-    function getBTC(uint i) public view returns(uint){
-        if(i == 0){
-            return(BTCUSD);}
-        else{
-            return(BTCUSD);   
-        }
-
-    }
-
-    function gettime(uint i) public view returns(uint){
-        if(i == 0){
-            return(time);}
-        else{
-            return(time);   
-        }
-
-    }
-
+    function gettime() public view returns(uint){
+        return(time);
+}
 
 
 }
