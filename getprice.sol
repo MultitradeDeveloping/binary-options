62793079ec8673c3ed65acb783e5b20e80c39c12
Mulrirtade <111198070+MultitradeDeveloping@users.noreply.github.com>
2023-05-14 01:28:24 +0300

    final

--- getprice.sol
+++ getprice.sol
@@ -1,31 +1,43 @@
+// import "./strings.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 
 contract Automaticaly {
-    
     AggregatorV3Interface internal BTCpriceFeed;
     AggregatorV3Interface internal ETHpriceFeed;
     AggregatorV3Interface internal LinkpriceFeed;
     uint public n = 0;
     uint public BTCUSD;
     uint time;
+    AggregatorV3Interface internal priceFeed;
+
+
     constructor() {
-        BTCpriceFeed = AggregatorV3Interface(0xA39434A63A52E749F02807ae27335515BA4b07F7);
-        ETHpriceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        LinkpriceFeed = AggregatorV3Interface(0x48731cF7e84dc94C5f84577882c14Be11a5B7456);
+        priceFeed = AggregatorV3Interface(
+            0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
+        );
     }
-    function LatestBTCprice() public{
+    /**
+    * Returns the latest price
+    */
+    function getLatestPrice() internal {
+        // prettier-ignore
         (
-            uint80 roundID,
+            /* uint80 roundID */,
             int price,
-            uint startedAt,
-            uint timeStamp,
-            uint80 answeredInRound
-        ) = BTCpriceFeed.latestRoundData();
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
         BTCUSD = uint(price);
-        time = timeStamp;
-        // time[n] = uint80(startedAt);
     }
 }
 
+    
 
 
 
