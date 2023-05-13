0d0f38d0460b3ba936640fa1c5b7bb5214c88ea4
Mulrirtade <111198070+MultitradeDeveloping@users.noreply.github.com>
2023-05-14 01:23:13 +0300

    Update fillorders.sol

--- fillorders.sol
+++ fillorders.sol
@@ -6,146 +6,145 @@ import "./orderbase.sol";
 
 contract orderSystem is orderbase{
 
-function limitbid(uint coef, uint x) public payable{
-    if(x == 0){x = n;}
-    uint t = block.timestamp;
-    uint treq = t%period;
-    require(treq>60);
-    id++;
-    oibids = oibids + msg.value;
+function limitBid(uint coef) public payable{
+    uint volume = msg.value ;
+    if(volume  == 0){volume = n;}
+    // uint t = block.timestamp;
+    // uint treq = t%period;
+    // require(treq>60);
+    id = id+1;
+    oibids = oibids + (msg.value*coef/1000);
     idBase[id] = msg.sender;
     bidIds[id] = coef;
-    bidMain[x][coef][id] = msg.value;
+    bidMain[n][coef][id] = msg.value;
+    // xx = askMain[n][coef][id];
     if(highestBid < coef){
         highestBid = coef;
-    }    
+    }   
 }
 
-
-function limitask(uint coef, uint x) public payable{
-    if(x == 0){x = n;}
-    uint t = block.timestamp;
-    uint treq = t%period;
-    require(treq>60);
-    id++;
-    oiasks = oiasks + msg.value;
-    idBase[id] = msg.sender; 
+function limitask(uint coef) public payable{
+    uint volume = msg.value ;
+    if(volume  == 0){volume = n;}
+    // uint t = block.timestamp;
+    // uint treq = t%period;
+    // require(treq>60);
+    id = id+1;
+    oiasks = oiasks + (msg.value*coef/1000);
+    idBase[id] = msg.sender;
     askIds[id] = coef;
-    askMain[x][coef][id] = msg.value;
-    if(lowestAsk == 0){
+    askMain[n][coef][id] = msg.value;
+    // xx = askMain[n][coef][id];
+    if(lowestAsk < coef){
         lowestAsk = coef;
-    }
-    if(lowestAsk > coef){
-        lowestAsk = coef;
-    }
+    }   
 }
 
-
-function marketAsk(uint x) public payable{
-    require(msg.value<=oibids, "Your order is more than supply in orderbook. Be careful to avoid squeeze");
-    oibids = oibids - msg.value;
-    id++;
-    idBase[id] = msg.sender; 
-    uint f = 0;
-    uint a;
-    if(x == 0){x = n;}
-    while(f<msg.value){
-        while(a<id){
-            a++;
-            //loc > supply at 1 order
-            uint loc = bidMain[x][highestBid][a];
-            if(loc > msg.value-f){
-                bidMain[x][highestBid][a] = bidMain[x][highestBid][a]-(msg.value-f);
-                askMain[x][highestBid][id] = msg.value-f;
-                filledAsks[id][highestBid] = msg.value-f;
-                filledBids[a][highestBid] = filledBids[a][highestBid] + (msg.value-f);
-                f = msg.value;
-            }
-
-            //loc <= supply at 1 order
-            else{
-                bidMain[x][highestBid][a] = 0;
-                askMain[x][highestBid][id] = msg.value-f;
-                filledBids[a][highestBid] = filledBids[a][highestBid] + msg.value-f;
-                filledAsks[id][highestBid] = msg.value-f;
-                f = f+loc;         
-            }
-
-               
-        }
-        if(f<msg.value){
-        a = 0;
-        renewHighestBids(x);
-        }
-        
-
-    }
-}
-
-function marketbid(uint x) public payable{
-
+function marketbid() public payable{
     require(msg.value<=oiasks, "Your order is more than supply in orderbook. Be careful to avoid squeeze");
     oiasks = oiasks - msg.value;
-    id++;
+    id = id+1;
     idBase[id] = msg.sender; 
     uint f = 0;
     uint a;
     while(f<msg.value){
         while(a<id){
-            a++;
-            //loc > supply at 1 order
-            uint loc = askMain[x][lowestAsk][a];
-            if(loc > msg.value-f){
-                askMain[x][lowestAsk][a] = askMain[x][lowestAsk][a]-(msg.value-f);
-                bidMain[x][lowestAsk][id] = msg.value-f;
-                filledBids[id][lowestAsk] = msg.value-f;
+            a = a+1;
+            //loc > supply at 1 order 
+            uint loc = askMain[n][lowestAsk][a];
+            if(loc >= msg.value-f){
+                askMain[n][lowestAsk][a] = askMain[n][lowestAsk][a]-(msg.value-f);
+                bidIds[id] = lowestAsk;
+                filledBids[id][lowestAsk] = msg.value;
                 filledAsks[a][lowestAsk] = filledAsks[a][lowestAsk] + (msg.value-f);
                 f = msg.value;
+
             }
 
             //loc <= supply at 1 order
             else{
-                askMain[x][lowestAsk][a] = 0;
-                bidMain[x][lowestAsk][id] = msg.value-f;
+                askMain[n][lowestAsk][a] = 0;
                 filledAsks[a][lowestAsk] = filledAsks[a][lowestAsk] + msg.value-f;
-                filledBids[id][lowestAsk] = msg.value-f;
-                f = f+loc;         
+                filledBids[id][lowestAsk] = msg.value;
+                f = f+loc; 
+                bidIds[id] = lowestAsk;
             }
-
                
         }
         if(f<msg.value){
         a = 0;
-        renewLowestAsk(x);
+        f = msg.value;
+        renewLowestAsk(msg.value);
         }
     }
 }
 
+function marketask() public payable{
+	 require(msg.value<=oibids, "Your order is more than supply in orderbook. Be careful to avoid squeeze");
+    oibids = oibids - msg.value;
+    id = id+1;
+    idBase[id] = msg.sender; 
+    uint f = 0;
+    uint a;
+    while(f<msg.value){
+        while(a<id){
+            a = a+1;
+            //loc > supply at 1 order 
+            uint loc = bidMain[n][highestBid][a];
+            if(loc >= msg.value-f){
+                bidMain[n][highestBid][a] = bidMain[n][highestBid][a]-(msg.value-f);
+                askIds[id] = highestBid;
+                filledAsks[id][highestBid] = msg.value;
+                filledBids[a][highestBid] = filledBids[a][highestBid] + (msg.value-f);
+                f = msg.value;
+
+            }
+
+            //loc <= supply at 1 order
+            else{
+                bidMain[n][highestBid][a] = 0;
+                filledBids[a][highestBid] = filledBids[a][highestBid] + msg.value-f;
+                filledAsks[id][highestBid] = msg.value;
+                f = f+loc; 
+                askIds[id] = highestBid;
+            }
+               
+        }
+        if(f<msg.value){
+        a = 0;
+        f = msg.value;
+        renewHighestBids(msg.value);
+        }
+    }
+	}
+	
 
 
 
-function cancelBid(uint value, uint coef, uint x) public {
+
+
+function cancelBid(uint value, uint coef, uint volume) public {
 uint i = 0;
 address payable addr;
-if(x == 0){x = n;}
+if(volume == 0){volume = n;}
 while(addr != msg.sender){
-    i++;
+    i = i+1;
     addr = payable(idBase[i]);
 }
-uint val = bidMain[x][i][coef];
+uint val = bidMain[n][highestBid][id];
 require(val>=value);
 addr.transfer(value);
 }
 
-function cancelAsk(uint value, uint coef, uint x) public {
+function cancelask(uint value, uint coef, uint volume) public {
 uint i = 0;
 address payable addr;
-if(x == 0){x = n;}
+if(volume == 0){volume = n;}
 while(addr != msg.sender){
-    i++;
+    i = i+1;
     addr = payable(idBase[i]);
 }
-uint val = askMain[x][i][coef];
+uint val = askMain[n][highestBid][id];
 require(val>=value);
 addr.transfer(value);
 }
