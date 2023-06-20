f271db2879aea641cc6ca8d30d36aead9b36597e
Mulrirtade <111198070+MultitradeDeveloping@users.noreply.github.com>
2023-06-21 00:23:02 +0300

    ready-to-testnet

--- cycle.sol
+++ cycle.sol
@@ -7,33 +7,39 @@ pragma solidity ^0.8.0;
 contract cycle is takeprofit{   
     address public owner = msg.sender;
 
-    function clean() public {
+
+    function clean() internal  {
     uint a ;
     while(a<=id){
         a = a+1;
-        if(askMain[n][a][askIds[a]] != 0){
+        if(askMain[n][askIds[id]][a] != 0){
             address payable addr = payable(idBase[a]);
-            addr.transfer(askMain[n][a][askIds[a]]);
+            addr.transfer(bidMain[n][askIds[id]][a]);
         }
-        if(bidMain[n][a][bidIds[a]] != 0){
+        if(bidMain[n][bidIds[id]][a] != 0){
             address payable addr = payable(idBase[a]);
-            addr.transfer(bidMain[n][a][bidIds[a]]);
+            addr.transfer(bidMain[n][bidIds[id]][a]);
         }
         idBase[a] = 0x0000000000000000000000000000000000000000;        
         askMain[n][askIds[a]][id] = 0;
         bidMain[n][bidIds[a]][id] = 0;
-        filledAsks[askIds[a]][a] = 0;
-        filledBids[bidIds[a]][a] = 0;
+        filledAsks[a][askIds[a]] = 0;
+        filledBids[a][bidIds[a]] = 0;
         askIds[a] = 0;
         bidIds[a] = 0;
+        // test++;
     }
     highestBid = 0;
     lowestAsk = 0;
+    oibids = 0;
+    oiasks = 0;
     id = 0;
     n = n+1;
     feeManagement();
     }
     
+    
+
 
 
     function feeManagement() internal{
@@ -49,7 +55,7 @@ contract cycle is takeprofit{
         uint treq = t%period;
         require(treq<60, "reverted by time requirement");
         uint last = BTCUSD;
-        // getLatestPrice(); //from getprice.sol 
+        getLatestPrice(); //from getprice.sol 
         if(BTCUSD>last){
             take(true);   
         }
