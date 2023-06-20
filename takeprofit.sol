59b44dce3fcb1920879014b277b28c5747124224
Mulrirtade <111198070+MultitradeDeveloping@users.noreply.github.com>
2023-06-21 00:27:27 +0300

    ready-to-testnet

--- takeprofit.sol
+++ takeprofit.sol
@@ -6,22 +6,14 @@ pragma solidity ^0.8.0;
 contract takeprofit is orderSystem{
 uint constant fee = 4;
 
-uint public test;
-uint public testi;
-uint public testii;
-
-
-function take(bool up) public {
+function take(bool up) internal  {
 if(up == true){
     uint i = 0;
     while (i<id){
         i++;
         address payable addr = payable(idBase[i]);
         uint coef = bidIds[i];
-        testii = coef;
         uint val = filledBids[i][coef];
-        testi = val+testi;
-        test = test + (val*(coef/1000+1)*(100-fee)/100);
         addr.transfer(val*(coef/1000+1)*(100-fee)/100);  
     }
 }
@@ -33,7 +25,7 @@ else{
         address payable addr = payable(idBase[i]);
         uint coef = askIds[i];
         uint val = filledAsks[i][coef];
-        addr.transfer(val*(coef/1000+1)*(100-fee)/100);
+        addr.transfer(val*(coef/1000+1)*(100-fee)/100);  
     }
 }
 
