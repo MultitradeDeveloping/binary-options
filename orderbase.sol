7d7d9c52f8c4399e7c202f3f99a90c49cc22fd8e
Mulrirtade <111198070+MultitradeDeveloping@users.noreply.github.com>
2023-06-21 00:26:55 +0300

    ready-to-testnet

--- orderbase.sol
+++ orderbase.sol
@@ -6,17 +6,17 @@ import  "./getprice.sol";
 contract orderbase is GetPrice{
     //due to the lack of numbers with a dot, all coefficients are stored multiplied by 1000
     
-    uint highestBid;
-    uint lowestAsk;
-    uint id;
-    uint oibids;
-    uint oiasks;
-    uint period = 86400;
+    uint public highestBid;
+    uint public lowestAsk;
+    uint public id;
+    uint public oibids;
+    uint public oiasks;
+    uint period = 60;
 
     //id to address
     mapping(uint => address) idBase;
 
-    //id => coef
+    //id => coefw
     mapping(uint => uint) askIds;
     mapping(uint => uint) bidIds;
 
@@ -26,35 +26,92 @@ contract orderbase is GetPrice{
     mapping(uint => mapping(uint => mapping(uint => uint))) bidMain;
 
 
-    function getHighestBid(uint x) public view returns(uint, uint){
-        uint c;
-        if(x == 0){x = n;}
-        for(uint a; a != id+1; a+=1){
-            c = c + bidMain[x][highestBid][a];
+    function calculateAverageAskCoef(uint volume) public view returns(uint){
+        if(volume > oiasks){return(0);}
+        else{
+            //spent by market order
+            uint spend;
+            uint fill;
+            uint highestUsedCoef;
+            while(spend < volume){
+                uint i;
+                uint localId;
+                uint LocalCoef = 65535;
+                while(i<=id){
+                    i++;
+                    if(askIds[i] <= LocalCoef && askIds[i] > highestUsedCoef && askMain[n][askIds[i]][i] != 0){
+                        LocalCoef = askIds[i];
+                        highestUsedCoef = LocalCoef;
+                        localId = i;
+                       }
+
+                }
+                
+                uint add = askMain[n][LocalCoef][localId]; 
+                if(add <= volume-spend){ 
+                    fill = fill+add;
+                    spend = spend + (add*LocalCoef/1000);
+                }
+                else{
+                    fill = fill + (volume - spend)*1000/LocalCoef;
+                    spend = volume;
+                }
+                
+            }   
+            return((1000*fill)/spend);
         }
-        return(highestBid, c);
+            
+
+    }
+
+    function calculateAverageBidCoef(uint volume) public view returns(uint){
+        if(volume > oibids){return(0);}
+        else{
+            //spent by market order
+            uint spend;
+            uint fill;
+            uint lowestUsedCoef;
+            while(spend < volume){
+                uint i;
+                uint localId;
+                uint LocalCoef;
+                while(i<=id){
+                    i++;
+                    if(bidIds[i] >= LocalCoef && bidIds[i] > lowestUsedCoef && bidMain[n][bidIds[i]][i] != 0){
+                        LocalCoef = bidIds[i];
+                        lowestUsedCoef = LocalCoef;
+                        localId = i;
+                       }
+
+                }
+                
+                uint add = bidMain[n][LocalCoef][localId]; 
+                if(add <= volume-spend){ 
+                    fill = fill+add;
+                    spend = spend + (add*LocalCoef/1000);
+                }
+                else{
+                    fill = fill + (volume - spend)*1000/LocalCoef;
+                    spend = volume;
+                }
+                
+            }
+            return((1000*fill)/spend);
+        }
+            
+
     }
 
 
-    function getLowestAsk(uint x) public view returns(uint coef, uint value){    
-        uint c;
-        if(x == 0){x = n;}
-        for(uint a; a != id+1; a+=1){
-            c = c + askMain[x][lowestAsk][a];
-        }
-        return(lowestAsk, c); 
-    }
 
-
-
-    function renewHighestBids(uint x) internal{
+    function renewHighestBids(uint volume) internal{
         uint i = 0;
         uint cf; 
-        if(x == 0){x = n;}
+        if(volume == 0){volume = n;}
         while(i != id){
-            i++;
+            i = i+1;
             uint loc = bidIds[i];
-            if(bidMain[x][loc][i] != 0){
+            if(bidMain[volume][loc][i] != 0){
                 if(cf == 0){
                     cf = loc;
                 }
@@ -62,19 +119,20 @@ contract orderbase is GetPrice{
                     cf = loc;
                 }
             }
+            if(i>=id){break;}
         }
         highestBid = cf;
     }
     
 
-    function renewLowestAsk(uint x) internal{
+    function renewLowestAsk(uint volume) internal{
         uint i = 0;
         uint cf;
-        if(x == 0){x = n;} 
+        if(volume == 0){volume = n;} 
         while(i != id){
-            i++;
+            i = i+1;
             uint loc = askIds[i];
-            if(askMain[x][loc][i] != 0){
+            if(askMain[volume][loc][i] != 0){
                 if(cf == 0){
                     cf = loc;
                 }
@@ -82,6 +140,8 @@ contract orderbase is GetPrice{
                     cf = loc;
                 }
             }
+
+            if(i>=id){break;}
         }
         lowestAsk = cf;
     }
